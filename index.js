5a45c21e0377232e48e34708b0aed1b52ee93cd2
Lucas Vieira <lucas.vieira@sirius.com.br>
2017-11-27 13:21:01 -0200

    Handling Routes

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const routes = require('./routes/api');
 
 //Set Up express App
-
 const app = express();
+
+// Initialize Routes
+app.use('/api',routes);
+
+//Listen for requests
+app.listen(process.env.port || 4000, function () {
+	console.log('Now Listening for Requests');
+});
